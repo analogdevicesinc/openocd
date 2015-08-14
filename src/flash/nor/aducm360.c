4651f5ad7aa18334bfa697ef30419d4afcf3883b
Michael Hennerich <michael.hennerich@analog.com>
2015-08-14 13:19:33 +0200

    src/flash/nor/aducm360: Add copyright
    This code was originally developed by Mikrosistemi Ltd. This was
    done under contract, and is owned by Analog Devices Inc.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- src/flash/nor/aducm360.c
+++ src/flash/nor/aducm360.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2015 by Ivan Buliev                                     *
+ *   Copyright (C) 2015 Analog Devices, by Ivan Buliev                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
