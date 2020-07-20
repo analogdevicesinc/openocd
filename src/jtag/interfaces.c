58df317db9b098fb75239b3836ea3eca8131e05b
ANALOG\CWentwo <chad.wentworth@analog.com>
2020-07-20 13:14:21 -0400

    ice-1000 support working with latest openocd

--- src/jtag/interfaces.c
+++ src/jtag/interfaces.c
@@ -147,6 +147,10 @@ extern struct adapter_driver stlink_dap_adapter_driver;
 #if BUILD_RSHIM == 1
 extern struct adapter_driver rshim_dap_adapter_driver;
 #endif
+#if BUILD_ICE_1000
+extern struct adapter_driver ice1000_adapter_driver;
+#endif
+
 #endif /* standard drivers */
 
 /**
@@ -263,6 +267,9 @@ struct adapter_driver *adapter_drivers[] = {
 #endif
 #if BUILD_RSHIM == 1
 		&rshim_dap_adapter_driver,
+#endif
+#if BUILD_ICE_1000 == 1
+		&ice1000_adapter_driver,
 #endif
 #endif /* standard drivers */
 		NULL,
