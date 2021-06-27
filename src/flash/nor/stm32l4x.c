analogdevicesinc/openocd

commit 33b479245b18ba1e86adb2025606c792def2199a

flash/nor: improved API of flash_driver.info & fixed buffer overruns
1) The API of "info" callback in "struct flash_driver" has been
improved. Fixed buffers for strings

2) Removed the calls to snprintf() from the flash_driver.info
implementations. Many of them were used in an unsafe manner
(buffer overruns were possible).

Change-Id: I42ab8a8018d01f9af43c5ba49f650c3cb5d31dcb
Signed-off-by: Jan Matyas <matyas@codasip.com>
Reviewed-on: http://openocd.zylin.com/6182
Tested-by: jenkins
Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>
Reviewed-by: Tomas Vanek <vanekt@fbl.cz>

src/flash/nor/stm32l4x.c
@@ -1332,13 +1332,36 @@ static int stm32l4_read_idcode(struct flash_bank *bank, uint32_t *id)
 	return (retval == ERROR_OK) ? ERROR_FAIL : retval;
 }
 
+static const char *get_stm32l4_rev_str(struct flash_bank *bank)
+{
+	struct stm32l4_flash_bank *stm32l4_info = bank->driver_priv;
+	const struct stm32l4_part_info *part_info = stm32l4_info->part_info;
+	assert(part_info);
+
+	const uint16_t rev_id = stm32l4_info->idcode >> 16;
+	for (unsigned int i = 0; i < part_info->num_revs; i++) {
+		if (rev_id == part_info->revs[i].rev)
+			return part_info->revs[i].str;
+	}
+	return "'unknown'";
+}
+
+static const char *get_stm32l4_bank_type_str(struct flash_bank *bank)
+{
+	struct stm32l4_flash_bank *stm32l4_info = bank->driver_priv;
+	assert(stm32l4_info->part_info);
+	assert(stm32l4_info->probed);
+	return stm32l4_is_otp(bank) ? "OTP" :
+			stm32l4_info->dual_bank_mode ? "Flash dual" :
+			"Flash single";
+}
+
 static int stm32l4_probe(struct flash_bank *bank)
 {
 	struct target *target = bank->target;
 	struct stm32l4_flash_bank *stm32l4_info = bank->driver_priv;
 	const struct stm32l4_part_info *part_info;
 	uint16_t flash_size_kb = 0xffff;
-	uint32_t device_id;
 	uint32_t options;
 
 	stm32l4_info->probed = false;
@@ -1348,7 +1371,9 @@ static int stm32l4_probe(struct flash_bank *bank)
 	if (retval != ERROR_OK)
 		return retval;
 
-	device_id = stm32l4_info->idcode & 0xFFF;
+	const uint32_t device_id = stm32l4_info->idcode & 0xFFF;
+	const uint16_t rev_id = stm32l4_info->idcode >> 16;
+	const char *rev_str = get_stm32l4_rev_str(bank);
 
 	for (unsigned int n = 0; n < ARRAY_SIZE(stm32l4_parts); n++) {
 		if (device_id == stm32l4_parts[n].id) {
@@ -1365,12 +1390,9 @@ static int stm32l4_probe(struct flash_bank *bank)
 	part_info = stm32l4_info->part_info;
 	stm32l4_info->flash_regs = stm32l4_info->part_info->default_flash_regs;
 
-	char device_info[1024];
-	retval = bank->driver->info(bank, device_info, sizeof(device_info));
-	if (retval != ERROR_OK)
-		return retval;
-
-	LOG_INFO("device idcode = 0x%08" PRIx32 " (%s)", stm32l4_info->idcode, device_info);
+	LOG_INFO("device idcode = 0x%08" PRIx32 " (%s - Rev %s : 0x%04x - %s-bank)",
+			stm32l4_info->idcode, part_info->device_str, rev_str, rev_id,
+			get_stm32l4_bank_type_str(bank));
 
 	/* read flash option register */
 	retval = stm32l4_read_flash_reg_by_index(bank, STM32_FLASH_OPTR_INDEX, &options);
@@ -1610,33 +1632,19 @@ static int stm32l4_auto_probe(struct flash_bank *bank)
 	return stm32l4_probe(bank);
 }
 
-static int get_stm32l4_info(struct flash_bank *bank, char *buf, int buf_size)
+static int get_stm32l4_info(struct flash_bank *bank, struct command_invocation *cmd)
 {
 	struct stm32l4_flash_bank *stm32l4_info = bank->driver_priv;
 	const struct stm32l4_part_info *part_info = stm32l4_info->part_info;
 
 	if (part_info) {
-		const char *rev_str = NULL;
-		uint16_t rev_id = stm32l4_info->idcode >> 16;
-		for (unsigned int i = 0; i < part_info->num_revs; i++) {
-			if (rev_id == part_info->revs[i].rev) {
-				rev_str = part_info->revs[i].str;
-				break;
-			}
-		}
-
-		int buf_len = snprintf(buf, buf_size, "%s - Rev %s : 0x%04x",
-				part_info->device_str, rev_str ? rev_str : "'unknown'", rev_id);
-
+		const uint16_t rev_id = stm32l4_info->idcode >> 16;
+		command_print_sameline(cmd, "%s - Rev %s : 0x%04x", part_info->device_str,
+				get_stm32l4_rev_str(bank), rev_id);
 		if (stm32l4_info->probed)
-			snprintf(buf + buf_len, buf_size - buf_len, " - %s-bank",
-					stm32l4_is_otp(bank) ? "OTP" :
-					stm32l4_info->dual_bank_mode ? "Flash dual" : "Flash single");
-
-		return ERROR_OK;
+			command_print_sameline(cmd, " - %s-bank", get_stm32l4_bank_type_str(bank));
 	} else {
-		snprintf(buf, buf_size, "Cannot identify target as an %s device", device_families);
-		return ERROR_FAIL;
+		command_print_sameline(cmd, "Cannot identify target as an %s device", device_families);
 	}
 
 	return ERROR_OK;

src/flash/nor/ambiqmicro.c
@@ -161,10 +161,9 @@ FLASH_BANK_COMMAND_HANDLER(ambiqmicro_flash_bank_command)
 	return ERROR_OK;
 }
 
-static int get_ambiqmicro_info(struct flash_bank *bank, char *buf, int buf_size)
+static int get_ambiqmicro_info(struct flash_bank *bank, struct command_invocation *cmd)
 {
 	struct ambiqmicro_flash_bank *ambiqmicro_info = bank->driver_priv;
-	int printed;
 	char *classname;
 
 	if (!ambiqmicro_info->probed) {
@@ -178,16 +177,12 @@ static int get_ambiqmicro_info(struct flash_bank *bank, char *buf, int buf_size)
 	else
 		classname = ambiqmicroClassname[0];
 
-	printed = snprintf(buf,
-		buf_size,
-		"\nAmbiq Micro information: Chip is "
+	command_print_sameline(cmd, "\nAmbiq Micro information: Chip is "
 		"class %d (%s) %s\n",
 		ambiqmicro_info->target_class,
 		classname,
 		ambiqmicro_info->target_name);
 
-	if ((printed < 0))
-		return ERROR_BUF_TOO_SMALL;
 	return ERROR_OK;
 }
 

src/flash/nor/at91sam3.c
@@ -256,7 +256,7 @@ static struct sam3_chip *get_current_sam3(struct command_invocation *cmd)
 
 	t = get_current_target(cmd->ctx);
 	if (!t) {
-		command_print(cmd, "No current target?");
+		command_print_sameline(cmd, "No current target?\n");
 		return NULL;
 	}
 
@@ -264,7 +264,7 @@ static struct sam3_chip *get_current_sam3(struct command_invocation *cmd)
 	if (!p) {
 		/* this should not happen */
 		/* the command is not registered until the chip is created? */
-		command_print(cmd, "No SAM3 chips exist?");
+		command_print_sameline(cmd, "No SAM3 chips exist?\n");
 		return NULL;
 	}
 
@@ -273,7 +273,7 @@ static struct sam3_chip *get_current_sam3(struct command_invocation *cmd)
 			return p;
 		p = p->next;
 	}
-	command_print(cmd, "Cannot find SAM3 chip?");
+	command_print_sameline(cmd, "Cannot find SAM3 chip?\n");
 	return NULL;
 }
 

src/flash/nor/at91sam4.c
@@ -235,7 +235,7 @@ static struct sam4_chip *get_current_sam4(struct command_invocation *cmd)
 
 	t = get_current_target(cmd->ctx);
 	if (!t) {
-		command_print(cmd, "No current target?");
+		command_print_sameline(cmd, "No current target?\n");
 		return NULL;
 	}
 
@@ -243,7 +243,7 @@ static struct sam4_chip *get_current_sam4(struct command_invocation *cmd)
 	if (!p) {
 		/* this should not happen */
 		/* the command is not registered until the chip is created? */
-		command_print(cmd, "No SAM4 chips exist?");
+		command_print_sameline(cmd, "No SAM4 chips exist?\n");
 		return NULL;
 	}
 
@@ -252,7 +252,7 @@ static struct sam4_chip *get_current_sam4(struct command_invocation *cmd)
 			return p;
 		p = p->next;
 	}
-	command_print(cmd, "Cannot find SAM4 chip?");
+	command_print_sameline(cmd, "Cannot find SAM4 chip?\n");
 	return NULL;
 }
 
@@ -2656,19 +2656,16 @@ static int sam4_GetDetails(struct sam4_bank_private *pPrivate)
 	return ERROR_OK;
 }
 
-static int sam4_info(struct flash_bank *bank, char *buf, int buf_size)
+static int sam4_info(struct flash_bank *bank, struct command_invocation *cmd)
 {
 	struct sam4_bank_private *pPrivate;
 	int k = bank->size / 1024;
 
 	pPrivate = get_sam4_bank_private(bank);
-	if (pPrivate == NULL) {
-		buf[0] = '\0';
+	if (pPrivate == NULL)
 		return ERROR_FAIL;
-	}
 
-	snprintf(buf, buf_size,
-		"%s bank %d: %d kB at " TARGET_ADDR_FMT,
+	command_print_sameline(cmd, "%s bank %d: %d kB at " TARGET_ADDR_FMT,
 		pPrivate->pChip->details.name,
 		pPrivate->bank_number,
 		k,

src/flash/nor/at91sam7.c
@@ -978,23 +978,17 @@ static int at91sam7_probe(struct flash_bank *bank)
 	return ERROR_OK;
 }
 
-static int get_at91sam7_info(struct flash_bank *bank, char *buf, int buf_size)
+static int get_at91sam7_info(struct flash_bank *bank, struct command_invocation *cmd)
 {
-	int printed;
 	struct at91sam7_flash_bank *at91sam7_info = bank->driver_priv;
 
 	if (at91sam7_info->cidr == 0)
 		return ERROR_FLASH_BANK_NOT_PROBED;
 
-	printed = snprintf(buf, buf_size,
-			"\n at91sam7 driver information: Chip is %s\n",
+	command_print_sameline(cmd, "\n at91sam7 driver information: Chip is %s\n",
 			at91sam7_info->target_name);
 
-	buf += printed;
-	buf_size -= printed;
-
-	printed = snprintf(buf,
-			buf_size,
+	command_print_sameline(cmd,
 			" Cidr: 0x%8.8" PRIx32 " | Arch: 0x%4.4x | Eproc: %s | Version: 0x%3.3x | "
 			"Flashsize: 0x%8.8" PRIx32 "\n",
 			at91sam7_info->cidr,
@@ -1003,31 +997,20 @@ static int get_at91sam7_info(struct flash_bank *bank, char *buf, int buf_size)
 			at91sam7_info->cidr_version,
 			bank->size);
 
-	buf += printed;
-	buf_size -= printed;
-
-	printed = snprintf(buf, buf_size,
-			" Master clock (estimated): %u KHz | External clock: %u KHz\n",
+	command_print_sameline(cmd,
+			" Master clock (estimated): %u kHz | External clock: %u kHz\n",
 			(unsigned)(at91sam7_info->mck_freq / 1000),
 			(unsigned)(at91sam7_info->ext_freq / 1000));
 
-	buf += printed;
-	buf_size -= printed;
-
-	printed = snprintf(buf,
-			buf_size,
+	command_print_sameline(cmd,
 			" Pagesize: %i bytes | Lockbits(%u): %i 0x%4.4x | Pages in lock region: %i\n",
 			at91sam7_info->pagesize,
 			bank->num_sectors,
 			at91sam7_info->num_lockbits_on,
 			at91sam7_info->lockbits,
-			at91sam7_info->pages_per_sector*at91sam7_info->num_lockbits_on);
+			at91sam7_info->pages_per_sector * at91sam7_info->num_lockbits_on);
 
-	buf += printed;
-	buf_size -= printed;
-
-	snprintf(buf, buf_size,
-		" Securitybit: %i | Nvmbits(%i): %i 0x%1.1x\n",
+	command_print_sameline(cmd, " Securitybit: %i | Nvmbits(%i): %i 0x%1.1x\n",
 		at91sam7_info->securitybit, at91sam7_info->num_nvmbits,
 		at91sam7_info->num_nvmbits_on, at91sam7_info->nvmbits);
 

src/flash/nor/ath79.c
@@ -875,17 +875,16 @@ static int ath79_protect_check(struct flash_bank *bank)
 	return ERROR_OK;
 }
 
-static int get_ath79_info(struct flash_bank *bank, char *buf, int buf_size)
+static int get_ath79_info(struct flash_bank *bank, struct command_invocation *cmd)
 {
 	struct ath79_flash_bank *ath79_info = bank->driver_priv;
 
 	if (!ath79_info->probed) {
-		snprintf(buf, buf_size,
-			 "\nATH79 flash bank not probed yet\n");
+		command_print_sameline(cmd, "\nATH79 flash bank not probed yet\n");
 		return ERROR_OK;
 	}
 
-	snprintf(buf, buf_size, "\nATH79 flash information:\n"
+	command_print_sameline(cmd, "\nATH79 flash information:\n"
 		"  Device \'%s\' (ID 0x%08" PRIx32 ")\n",
 		ath79_info->dev->name, ath79_info->dev->device_id);
 

src/flash/nor/atsamv.c
@@ -607,7 +607,7 @@ static int samv_write(struct flash_bank *bank, const uint8_t *buffer,
 	return ERROR_OK;
 }
 
-static int samv_get_info(struct flash_bank *bank, char *buf, int buf_size)
+static int samv_get_info(struct flash_bank *bank, struct command_invocation *cmd)
 {
 	struct samv_flash_bank *samv_info = bank->driver_priv;
 	if (!samv_info->probed) {
@@ -615,7 +615,7 @@ static int samv_get_info(struct flash_bank *bank, char *buf, int buf_size)
 		if (ERROR_OK != r)
 			return r;
 	}
-	snprintf(buf, buf_size, "Cortex-M7 detected with %" PRIu32 " kB flash",
+	command_print_sameline(cmd, "Cortex-M7 detected with %" PRIu32 " kB flash\n",
 			bank->size / 1024);
 	return ERROR_OK;
 }

src/flash/nor/avrf.c
@@ -367,7 +367,7 @@ static int avrf_auto_probe(struct flash_bank *bank)
 	return avrf_probe(bank);
 }
 
-static int avrf_info(struct flash_bank *bank, char *buf, int buf_size)
+static int avrf_info(struct flash_bank *bank, struct command_invocation *cmd)
 {
 	struct target *target = bank->target;
 	struct avr_common *avr = target->arch_info;
@@ -400,12 +400,12 @@ static int avrf_info(struct flash_bank *bank, char *buf, int buf_size)
 
 	if (avr_info != NULL) {
 		/* chip found */
-		snprintf(buf, buf_size, "%s - Rev: 0x%" PRIx32 "", avr_info->name,
+		command_print_sameline(cmd, "%s - Rev: 0x%" PRIx32 "", avr_info->name,
 			EXTRACT_VER(device_id));
 		return ERROR_OK;
 	} else {
 		/* chip not supported */
-		snprintf(buf, buf_size, "Cannot identify target as a avr\n");
+		command_print_sameline(cmd, "Cannot identify target as a avr\n");
 		return ERROR_FLASH_OPERATION_FAILED;
 	}
 }

commit 212c1929eb26ed5d63e0fe05005b8901b3f81efc

flash/nor/stm32l4: Fix stm32l4_probe()
The current implementation fails due to the assert() statements in
get_stm32l4_rev_str() and get_stm32l4_bank_type_str(). Rearrange the
code in order to fix the problem.

Change-Id: If19c648dec8ddd3ef2fb801150114104b34c3bf2
Signed-off-by: Marc Schink <dev@zapb.de>
Fixes: 33b479245b ("flash/nor: improved API of flash_driver.info & fixed buffer overruns")
Reviewed-on: http://openocd.zylin.com/6326
Tested-by: jenkins
Reviewed-by: Jan Matyas <matyas@codasip.com>
Reviewed-by: Tarek BOCHKATI <tarek.bouchkati@gmail.com>
Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>

src/flash/nor/stm32l4x.c
@@ -1350,7 +1350,6 @@ static const char *get_stm32l4_bank_type_str(struct flash_bank *bank)
 {
 	struct stm32l4_flash_bank *stm32l4_info = bank->driver_priv;
 	assert(stm32l4_info->part_info);
-	assert(stm32l4_info->probed);
 	return stm32l4_is_otp(bank) ? "OTP" :
 			stm32l4_info->dual_bank_mode ? "Flash dual" :
 			"Flash single";
@@ -1372,8 +1371,6 @@ static int stm32l4_probe(struct flash_bank *bank)
 		return retval;
 
 	const uint32_t device_id = stm32l4_info->idcode & 0xFFF;
-	const uint16_t rev_id = stm32l4_info->idcode >> 16;
-	const char *rev_str = get_stm32l4_rev_str(bank);
 
 	for (unsigned int n = 0; n < ARRAY_SIZE(stm32l4_parts); n++) {
 		if (device_id == stm32l4_parts[n].id) {
@@ -1388,11 +1385,14 @@ static int stm32l4_probe(struct flash_bank *bank)
 	}
 
 	part_info = stm32l4_info->part_info;
-	stm32l4_info->flash_regs = stm32l4_info->part_info->default_flash_regs;
+	const char *rev_str = get_stm32l4_rev_str(bank);
+	const uint16_t rev_id = stm32l4_info->idcode >> 16;
 
 	LOG_INFO("device idcode = 0x%08" PRIx32 " (%s - Rev %s : 0x%04x - %s-bank)",
 			stm32l4_info->idcode, part_info->device_str, rev_str, rev_id,
 			get_stm32l4_bank_type_str(bank));
+
+	stm32l4_info->flash_regs = stm32l4_info->part_info->default_flash_regs;
 
 	/* read flash option register */
 	retval = stm32l4_read_flash_reg_by_index(bank, STM32_FLASH_OPTR_INDEX, &options);