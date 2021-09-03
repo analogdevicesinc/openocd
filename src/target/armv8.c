5b769915d2444c9297d092106efcf28e70367ee2
Mike Harrington <michael.harrington@analog.com>
2021-09-03 15:25:50 -0400

    Exception levels have access to all registers in lower exception levels. I.e. EL3 can access EL0-EL2.

--- src/target/armv8.c
+++ src/target/armv8.c
@@ -124,7 +124,7 @@ const char *armv8_mode_name(unsigned psr_mode)
 
 static int instr_read_data_r0_64(struct arm_dpm *dpm, uint32_t opcode, uint64_t *data_64, uint32_t expected_el)
 {
-	if (armv8_curel_from_core_mode(dpm->arm->core_mode) != expected_el) {
+	if (armv8_curel_from_core_mode(dpm->arm->core_mode) < expected_el) {
 		return ERROR_TARGET_EXCEPTION_LEVEL;
 	}
 
@@ -133,7 +133,7 @@ static int instr_read_data_r0_64(struct arm_dpm *dpm, uint32_t opcode, uint64_t 
 
 static int instr_read_data_r0(struct arm_dpm *dpm, uint32_t opcode, uint64_t *data_64, uint32_t expected_el)
 {
-	if (armv8_curel_from_core_mode(dpm->arm->core_mode) != expected_el) {
+	if (armv8_curel_from_core_mode(dpm->arm->core_mode) < expected_el) {
 		return ERROR_TARGET_EXCEPTION_LEVEL;
 	}
 
@@ -145,7 +145,7 @@ static int instr_read_data_r0(struct arm_dpm *dpm, uint32_t opcode, uint64_t *da
 
 static int instr_read_data_r0_32(struct arm_dpm *dpm, uint32_t opcode, uint32_t *data, uint32_t expected_el)
 {
-	if (armv8_curel_from_core_mode(dpm->arm->core_mode) != expected_el) {
+	if (armv8_curel_from_core_mode(dpm->arm->core_mode) < expected_el) {
 		return ERROR_TARGET_EXCEPTION_LEVEL;
 	}
 
@@ -154,7 +154,7 @@ static int instr_read_data_r0_32(struct arm_dpm *dpm, uint32_t opcode, uint32_t 
 
 static int instr_read_data_dcc(struct arm_dpm *dpm, uint32_t opcode, uint32_t *data, uint32_t expected_el)
 {
-	if (armv8_curel_from_core_mode(dpm->arm->core_mode) != expected_el) {
+	if (armv8_curel_from_core_mode(dpm->arm->core_mode) < expected_el) {
 		return ERROR_TARGET_EXCEPTION_LEVEL;
 	}
 
