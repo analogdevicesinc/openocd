41236b14bd904003de51ac96366fc51dbb62ab88
boban <Boban.Pallathucharry@analog.com>
2021-10-15 12:36:59 -0400

    check argument count for log_debug, log_error and log_info commands

--- src/helper/log.c
+++ src/helper/log.c
@@ -293,14 +293,22 @@ COMMAND_HANDLER(handle_log_output_command)
 
 COMMAND_HANDLER(handle_log_info_command)
 {
+	if (CMD_ARGC == 0) {
+		LOG_ERROR("Insufficient number of arguments\nCheck command usage");
+		return ERROR_COMMAND_SYNTAX_ERROR;
+	}
 	LOG_INFO("%s", CMD_ARGV[0]);
 	return ERROR_OK;
-
+	
 	return ERROR_COMMAND_SYNTAX_ERROR;
 }
 
 COMMAND_HANDLER(handle_log_error_command)
 {
+	if (CMD_ARGC == 0) {
+		LOG_ERROR("Insufficient number of arguments\nCheck command usage");
+		return ERROR_COMMAND_SYNTAX_ERROR;
+	}
 	LOG_ERROR("%s", CMD_ARGV[0]);
 	return ERROR_OK;
 
@@ -309,6 +317,10 @@ COMMAND_HANDLER(handle_log_error_command)
 
 COMMAND_HANDLER(handle_log_debug_command)
 {
+	if (CMD_ARGC == 0) {
+		LOG_ERROR("Insufficient number of arguments\nCheck command usage");
+		return ERROR_COMMAND_SYNTAX_ERROR;
+	}
 	LOG_DEBUG("%s", CMD_ARGV[0]);
 	return ERROR_OK;
 
