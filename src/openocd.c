398ef28b3fcf4cc6a1891177aaf416f0afdbee0a
boban <Boban.Pallathucharry@analog.com>
2021-10-22 12:04:43 -0400

    removed implied option '-c' from command usage

--- src/openocd.c
+++ src/openocd.c
@@ -237,7 +237,7 @@ static const struct command_registration openocd_command_handlers[] = {
 		.handler = &handle_firmware_command,
 		.mode = COMMAND_CONFIG,
 		.help = "Set the firmware to be loaded.",
-		.usage = "-c \"firmware <filepath>\""
+		.usage = "\"firmware <filepath>\""
 	},
 	COMMAND_REGISTRATION_DONE
 };
