e7ac32d3cb5de31b55b9c7ed0d947bae8b57edc1
boban <Boban.Pallathucharry@analog.com>
2021-10-22 15:20:12 -0400

    reverted .usage string to original value

--- src/openocd.c
+++ src/openocd.c
@@ -237,7 +237,7 @@ static const struct command_registration openocd_command_handlers[] = {
 		.handler = &handle_firmware_command,
 		.mode = COMMAND_CONFIG,
 		.help = "Set the firmware to be loaded.",
-		.usage = "\"firmware <filepath>\""
+		.usage = "filename"
 	},
 	COMMAND_REGISTRATION_DONE
 };
