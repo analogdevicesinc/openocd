cce0628783803b993d5aac1c2711eee0b357396d
Kevin Gillespie <kevin.gillespie@maximintegrated.com>
2021-12-16 15:05:51 -0600

    Fixing includes.

--- src/flash/nor/max32xxx.c
+++ src/flash/nor/max32xxx.c
@@ -26,6 +26,7 @@
 #include <target/armv7m.h>
 #include <target/target.h>
 #include <target/target_type.h>
+#include "helper/binarybuffer.h"
 
 /* Register addresses */
 #define FLC_ADDR				0x00000000
@@ -112,7 +113,7 @@ struct max32xxx_flash_bank {
 };
 
 static const uint8_t write_code_arm[] = {
-#include "../../contrib/loaders/flash/max32xxx/max32xxx_write_arm.inc"
+#include "contrib/loaders/flash/max32xxx/max32xxx_write_arm.inc"
 };
 
 FLASH_BANK_COMMAND_HANDLER(max32xxx_flash_bank_command)
