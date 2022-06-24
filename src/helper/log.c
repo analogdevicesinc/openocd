4c05fce730f28e342d66db04c5e03f96af73a349
Boban Pallathucharry <Boban.Pallathucharry@analog.com>
2022-06-24 12:00:17 -0400

    increasing keep_alive timeout to avoid the warning

--- src/helper/log.c
+++ src/helper/log.c
@@ -503,7 +503,7 @@ char *alloc_printf(const char *format, ...)
  *
  */
 #define KEEP_ALIVE_KICK_TIME_MS  500
-#define KEEP_ALIVE_TIMEOUT_MS   1000
+#define KEEP_ALIVE_TIMEOUT_MS   5000
 
 static void gdb_timeout_warning(int64_t delta_time)
 {
