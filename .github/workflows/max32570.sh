c4ef03372c1bb023da7f77993b20729f01db077a
kevin-gillespie <kevin.gillespie@maximintegrated.com>
2022-12-16 10:20:27 -0600

    Passing SWD_SN as an argument. (#12)
    * Passing SWD_SN as an argument.
    
    * Adding concurrency checks.
    
    * Reverting ubuntu version for snapshot workflow.
    
    Co-authored-by: Kevin Gillespie <Kevin.Gillespie@analog.com>

--- .github/workflows/max32570.sh
+++ .github/workflows/max32570.sh
@@ -1,8 +1,14 @@
 TARGET=max32570
 TARGET_CFG=${TARGET}.cfg
-CMSIS_DAP_ID=0409000080fe82cc00000000000000000000000097969906
-HEXGEN=/home/btm-ci/Tools/hexgen.pl
-# HEXGEN=~/Tools/hexgen.pl
+
+if [[ $# -ne 1 ]]
+then
+    echo "Need to pass in CMSIS_DAP_ID as argument"
+    exit 1
+fi
+
+CMSIS_DAP_ID=$1 # Get from arguments
+HEXGEN=~/Tools/hexgen.pl
 
 # Reset any tcl file modifications
 git checkout tcl/target/${TARGET_CFG}
@@ -12,15 +18,19 @@ sed -i "s/set QSPI_ENABLE 0/set QSPI_ENABLE 1/g" tcl/target/${TARGET_CFG}
 
 # Test 1: Start a simple OpenOCD session
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; reset halt; exit"
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; reset halt; exit"
 
 # Test 2: Mass erase
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
--c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; reset halt; max32xxx mass_erase 0; flash erase_check 0; exit"
+-c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+-c "adapter serial ${CMSIS_DAP_ID}" -c "init; reset halt; max32xxx mass_erase 0; flash erase_check 0; exit"
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
--c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; reset halt; max32xxx mass_erase 1; flash erase_check 1; exit"
+-c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+-c "adapter serial ${CMSIS_DAP_ID}" -c "init; reset halt; max32xxx mass_erase 1; flash erase_check 1; exit"
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
--c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; reset halt; max32xxx_qspi mass_erase 2; flash erase_check 2; exit"
+-c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+-c "adapter serial ${CMSIS_DAP_ID}" -c "init; reset halt; max32xxx_qspi mass_erase 2; flash erase_check 2; exit"
 
 # Test 3: Program and verify a file to the main flash bank
 export ADDR_BASE=0x10000000
@@ -29,7 +39,8 @@ export LEN=0x10000
 export LEN_MOD=0x10000
 $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
 
 # Test 4: Program and verify a file to the second flash bank
 export ADDR_BASE=0x10080000
@@ -38,7 +49,8 @@ export LEN=0x10000
 export LEN_MOD=0x10000
 $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
 
 # Test 5: Program and verify a file to the QSPI bank
 export ADDR_BASE=0x08000000
@@ -47,7 +59,8 @@ export LEN=0x1000
 export LEN_MOD=0x1000
 $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
 
 # Test 6: Program across multiple internal flash banks
 export ADDR_BASE=0x10000000
@@ -56,7 +69,8 @@ export LEN=0x100000
 export LEN_MOD=0x0
 $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
 
 # Test 7: Program and verify a file to the QSPI bank with encryption
 export ADDR_BASE=0x08000000
@@ -68,7 +82,8 @@ $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 # Program the flash encrypted
 sed -i "s/set QSPI_OPTIONS 0x0/set QSPI_OPTIONS 0x22/g" tcl/target/${TARGET_CFG}
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; reset halt; program ${TARGET}_rand.hex verify exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; reset halt; program ${TARGET}_rand.hex verify exit "
 
 # Revert the options to disable encryption
 sed -i "s/set QSPI_OPTIONS 0x22/set QSPI_OPTIONS 0x0/g" tcl/target/${TARGET_CFG}
@@ -76,7 +91,8 @@ sed -i "s/set QSPI_OPTIONS 0x22/set QSPI_OPTIONS 0x0/g" tcl/target/${TARGET_CFG}
 # Verify unencrypted, expect this to fail
 set +e
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; reset halt; flash verify_image ${TARGET}_rand.hex; exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; reset halt; flash verify_image ${TARGET}_rand.hex; exit "
 
 # Check the return value to make sure we received an error
 if [ "$?" -ne "1" ]
@@ -96,7 +112,8 @@ $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 # Program the flash encrypted and authenticated
 sed -i "s/set QSPI_OPTIONS 0x0/set QSPI_OPTIONS 0x26/g" tcl/target/${TARGET_CFG}
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-  -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; reset halt; program ${TARGET}_rand.hex verify exit "
+  -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+  -c "adapter serial ${CMSIS_DAP_ID}" -c "init; reset halt; program ${TARGET}_rand.hex verify exit "
 
 # Revert the options to disable authentication
 sed -i "s/set QSPI_OPTIONS 0x26/set QSPI_OPTIONS 0x22/g" tcl/target/${TARGET_CFG}
@@ -104,7 +121,8 @@ sed -i "s/set QSPI_OPTIONS 0x26/set QSPI_OPTIONS 0x22/g" tcl/target/${TARGET_CFG
 # Verify unencrypted, expect this to fail
 set +e
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-  -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; reset halt; flash verify_image ${TARGET}_rand.hex; exit "
+  -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+  -c "adapter serial ${CMSIS_DAP_ID}" -c "init; reset halt; flash verify_image ${TARGET}_rand.hex; exit "
 
 # Check the return value to make sure we received an error
 if [ "$?" -ne "1" ]
@@ -119,7 +137,8 @@ sed -i "s/set QSPI_OPTIONS 0x22/set QSPI_OPTIONS 0x0/g" tcl/target/${TARGET_CFG}
 # Verify unencrypted, expect this to fail
 set +e
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-  -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; reset halt; flash verify_image ${TARGET}_rand.hex; exit "
+  -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+  -c "adapter serial ${CMSIS_DAP_ID}" -c "init; reset halt; flash verify_image ${TARGET}_rand.hex; exit "
 
 # Check the return value to make sure we received an error
 if [ "$?" -ne "1" ]
@@ -137,7 +156,8 @@ export LEN=0x1
 export LEN_MOD=0x0
 $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
 
 export ADDR_BASE=0x10000000
 export ADDR_MOD=0x1000
@@ -145,7 +165,8 @@ export LEN=0x3
 export LEN_MOD=0x5
 $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
 
 # Test 10: Program small flash sections in QSPI
 sed -i "s/set QSPI_OPTIONS 0x0/set QSPI_OPTIONS 0x26/g" tcl/target/${TARGET_CFG}
@@ -155,7 +176,8 @@ export LEN=0x1
 export LEN_MOD=0x0
 $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
 
 export ADDR_BASE=0x08000000
 export ADDR_MOD=0x1000
@@ -163,6 +185,10 @@ export LEN=0x3
 export LEN_MOD=0x5
 $HEXGEN $ADDR_BASE $ADDR_MOD $LEN $LEN_MOD ${TARGET}_rand.hex
 ./src/openocd -f tcl/interface/cmsis-dap.cfg -f tcl/target/${TARGET_CFG} -s tcl  \
-    -c "cmsis_dap_serial  ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
+    -c "gdb_port 3334" -c "telnet_port 4445" -c "tcl_port 6665" \
+    -c "adapter serial ${CMSIS_DAP_ID}" -c "init; program ${TARGET}_rand.hex verify exit "
+
+# Reset any tcl file modifications
+git checkout tcl/target/${TARGET_CFG}
 
 echo "Success!"
