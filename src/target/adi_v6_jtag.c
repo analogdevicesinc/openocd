fda922640cdaa0c8bb54869a0f6b332f6e89d4b3
Adam Lofthouse-Hill <Adam.Lofthouse-Hill@analog.com>
2023-02-17 09:11:54 +0000

    Updating copyright year

--- src/target/adi_v6_jtag.c
+++ src/target/adi_v6_jtag.c
@@ -12,6 +12,8 @@
  *
  *   Copyright (C) 2019, Ampere Computing LLC
  *
+ *   Copyright (C) 2023, Analog Devices, Inc.
+ *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
