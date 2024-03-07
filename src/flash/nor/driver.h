e16d065411fa24de7eb85ecf9bc2a955345d2fd6
Steve Mcgrath <Steve.Mcgrath@analog.com>
2024-03-07 13:26:22 -0500

    Consolidating drivers into single file to use same driver functions. Omit NOR flash on carrier board. Include helper cfg file for algorithm and param file finding.

--- src/flash/nor/driver.h
+++ src/flash/nor/driver.h
@@ -240,7 +240,6 @@ const struct flash_driver *flash_driver_find_by_name(const char *name);
 extern const struct flash_driver adsp2183x_flash;
 extern const struct flash_driver adsp2183x_hyperflash;
 extern const struct flash_driver adsp2183x_otp;
-extern const struct flash_driver adsp2183x_xspi_norflash;
 extern const struct flash_driver adspsc59x_flash;
 extern const struct flash_driver adspsc59x_a55_flash;
 extern const struct flash_driver aduc702x_flash;
