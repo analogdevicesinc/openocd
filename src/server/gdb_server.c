7c9c6df05db982629f4cacc1b5b186b0df44dda2
Paul Fertser <fercerpav@gmail.com>
2024-04-25 14:33:07 +0300

    gdb_server: do not start multiple instances on "pipe"
    For configurations which include multiple targets and the "pipe" mode is
    requested only the first gdb_server instance should be enabled,
    otherwise GDB gets confusing replies, goes out of sync and the session
    fails in weird ways.
    
    Compile-tested only.
    
    Signed-off-by: Paul Fertser <fercerpav@gmail.com>
    Change-Id: If8f13aa7b58e9b0dc6d5ae88cf75538b34cc1218
    Reviewed-on: https://review.openocd.org/c/openocd/+/8222
    Tested-by: jenkins
    Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/server/gdb_server.c
+++ src/server/gdb_server.c
@@ -3881,7 +3881,7 @@ static int gdb_target_add_one(struct target *target)
 		return gdb_target_start(target, target->gdb_port_override);
 	}
 
-	if (strcmp(gdb_port, "disabled") == 0) {
+	if (strcmp(gdb_port_next, "disabled") == 0) {
 		LOG_INFO("gdb port disabled");
 		return ERROR_OK;
 	}
@@ -3910,6 +3910,8 @@ static int gdb_target_add_one(struct target *target)
 					gdb_port_next = strdup("0");
 				}
 			}
+		} else if (strcmp(gdb_port_next, "pipe") == 0) {
+			gdb_port_next = "disabled";
 		}
 	}
 	return retval;
