analogdevicesinc/openocd

commit 15dad1043fd9662d45aa4453a4d651959c9f3edd

itm: rework itm commands before 'init'
If the command 'itm port[s]' is issued before 'init', the ITM can
not be programmed because OpenOCD cannot access the target yet.
The configuration is recorded and applied after target examine.
The current test to trigger the ITM delayed-programming is based
on the TPIU configuration. This is allowed because the only use of
ITM it so send data through TPIU.
In case of system TPIU, not belonging anymore to the target, there
is no more association between target ITM and system TPIU.

Add a flag to record the pending ITM configuration requested
before 'init' and test the flag to trigger the delayed-programming
of the ITM.

Change-Id: I101eb97a116d7925cd2ff068f3e8813fc008b08e
Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
Reviewed-on: http://openocd.zylin.com/6029
Tested-by: jenkins

src/target/armv7m_trace.c
@@ -408,8 +408,9 @@ COMMAND_HANDLER(handle_itm_port_command)
 
 	if (CMD_CTX->mode == COMMAND_EXEC)
 		return armv7m_trace_itm_config(target);
-	else
-		return ERROR_OK;
+
+	armv7m->trace_config.itm_deferred_config = true;
+	return ERROR_OK;
 }
 
 COMMAND_HANDLER(handle_itm_ports_command)
@@ -427,8 +428,9 @@ COMMAND_HANDLER(handle_itm_ports_command)
 
 	if (CMD_CTX->mode == COMMAND_EXEC)
 		return armv7m_trace_itm_config(target);
-	else
-		return ERROR_OK;
+
+	armv7m->trace_config.itm_deferred_config = true;
+	return ERROR_OK;
 }
 
 static const struct command_registration tpiu_command_handlers[] = {

src/target/armv7m_trace.h
@@ -78,6 +78,8 @@ struct armv7m_trace_config {
 	bool itm_async_timestamps;
 	/** Enable synchronisation packet transmission (for sync port only) */
 	bool itm_synchro_packets;
+	/** Config ITM after target examine */
+	bool itm_deferred_config;
 
 	/** Current frequency of TRACECLKIN (usually matches HCLK) */
 	unsigned int traceclkin_freq;

src/target/cortex_m.c
@@ -2082,10 +2082,11 @@ int cortex_m_examine(struct target *target)
 		if (retval != ERROR_OK)
 			return retval;
 
-		if (armv7m->trace_config.config_type != TRACE_CONFIG_TYPE_DISABLED) {
+		if (armv7m->trace_config.config_type != TRACE_CONFIG_TYPE_DISABLED)
 			armv7m_trace_tpiu_config(target);
+
+		if (armv7m->trace_config.itm_deferred_config)
 			armv7m_trace_itm_config(target);
-		}
 
 		/* NOTE: FPB and DWT are both optional. */
 

commit 1c76088c3698f3922fe13e6220b7ada692cead8b

itm: fix default initialization
Commit 15dad1043fd9 ("itm: rework itm commands before 'init'")
ignores the default enable of ITM channel 0, that is applied when
no 'itm port[s]' is issued.

Call armv7m_trace_itm_config() unconditionally to handle it.

Change-Id: I3e85d0b063ed38c1552f6af9ea9eea2e76aa9025
Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
Reported-by: Paul Fertser <fercerpav@gmail.com>
Fixes: 15dad1043fd9 ("itm: rework itm commands before 'init'")
Reviewed-on: https://review.openocd.org/c/openocd/+/7900
Reviewed-by: <post@frankplowman.com>
Tested-by: jenkins

src/target/armv7m_trace.c
@@ -92,11 +92,14 @@ COMMAND_HANDLER(handle_itm_port_command)
 	else
 		armv7m->trace_config.itm_ter[reg_idx] &= ~(1 << port);
 
-	if (CMD_CTX->mode == COMMAND_EXEC)
-		return armv7m_trace_itm_config(target);
+	/*
+	 * In config mode ITM is not accessible yet.
+	 * Keep the value and it will be programmed at target init.
+	 */
+	if (CMD_CTX->mode == COMMAND_CONFIG)
+		return ERROR_OK;
 
-	armv7m->trace_config.itm_deferred_config = true;
-	return ERROR_OK;
+	return armv7m_trace_itm_config(target);
 }
 
 COMMAND_HANDLER(handle_itm_ports_command)
@@ -112,11 +115,14 @@ COMMAND_HANDLER(handle_itm_ports_command)
 	memset(armv7m->trace_config.itm_ter, enable ? 0xff : 0,
 	       sizeof(armv7m->trace_config.itm_ter));
 
-	if (CMD_CTX->mode == COMMAND_EXEC)
-		return armv7m_trace_itm_config(target);
+	/*
+	 * In config mode ITM is not accessible yet.
+	 * Keep the value and it will be programmed at target init.
+	 */
+	if (CMD_CTX->mode == COMMAND_CONFIG)
+		return ERROR_OK;
 
-	armv7m->trace_config.itm_deferred_config = true;
-	return ERROR_OK;
+	return armv7m_trace_itm_config(target);
 }
 
 static const struct command_registration itm_command_handlers[] = {

src/target/armv7m_trace.h
@@ -35,8 +35,6 @@ struct armv7m_trace_config {
 	bool itm_async_timestamps;
 	/** Enable synchronisation packet transmission (for sync port only) */
 	bool itm_synchro_packets;
-	/** Config ITM after target examine */
-	bool itm_deferred_config;
 };
 
 extern const struct command_registration armv7m_trace_command_handlers[];

src/target/cortex_m.c
@@ -2659,8 +2659,8 @@ int cortex_m_examine(struct target *target)
 		if (retval != ERROR_OK)
 			return retval;
 
-		if (armv7m->trace_config.itm_deferred_config)
-			armv7m_trace_itm_config(target);
+		/* Configure ITM */
+		armv7m_trace_itm_config(target);
 
 		/* NOTE: FPB and DWT are both optional. */
 