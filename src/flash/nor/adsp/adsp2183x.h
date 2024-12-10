767ab07aa40b99b07a285d2c9f41c33350ca3145
Mcgrath, Steve <Steve.Mcgrath@analog.com>
2024-12-10 13:49:11 -0500

    Add flash driver support for 57x processors with a5 core. (#59)
    * Add flash driver support for 57x processors with a5 core.
    
    Updates to current flash driver structure that differentiates between buffer size and sector size. New buffer size attribute is read from the algorithm binary and used as a maximum parameter that is provided to the running algorithm application. This attribute is used for both the read and write flash operations. Sector size is continued to be used for sector erase operation.
    
    * Add header protections

--- src/flash/nor/adsp/adsp2183x.h
+++ src/flash/nor/adsp/adsp2183x.h
@@ -1,6 +1,8 @@
 // SPDX-License Identifier: GPL-2.0-or-later
 // Copyright (C) 2022-2024 Analog Devices, Inc.
 
+#ifndef ADSP2183X_H
+#define ADSP2183X_H
 
 struct custom_algorithm {
 	uint8_t *adsp2183x_algo;
@@ -10,9 +12,10 @@ struct custom_algorithm {
 	unsigned long buffer_address;	  /*  address and algo parameter address (g_cfg)*/
 	unsigned long version;
 	unsigned long size;
+	unsigned long buffer_size
 };
 
-#define PARAMETER_FILE_COUNT 5
+#define PARAMETER_FILE_COUNT 6
 
 #define SPI_NAME "adsp2183x.spi"
 
@@ -55,3 +58,5 @@ struct adsp2183x_algo_params {
 	uint8_t ready[4];
 	uint8_t device_id[4];
 };
+
+#endif // ADSP2183X_H
