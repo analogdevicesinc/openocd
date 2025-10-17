b939136f52c9759f2c3b05a65ed8707736546524
Henrik Mau <henrik.mau@analog.com>
2025-10-17 15:57:38 +0100

    target/rvmax: Initialise the list_head structs before using them

--- src/target/rvmax/rvmax.c
+++ src/target/rvmax/rvmax.c
@@ -46,8 +46,9 @@
 #define RVMAX_VERSION_MAX 0
 #define RVMAX_VERSION_MIN 10
 
-struct list_head rm_tap_list;
-struct list_head rm_du_list;
+OOCD_LIST_HEAD(rm_tap_list);
+OOCD_LIST_HEAD(rm_du_list);
+
 extern uint32_t rvmax_auth_data[];
 char rvmax_driver_info[256] = "";
 static int rvmax_remove_breakpoint(struct target *target,
