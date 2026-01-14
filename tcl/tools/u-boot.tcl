e2aeb4e05652abb183e0cef8ead451a63f702b26
Ozan Durgut <ozan.durgut@analog.com>
2026-01-14 14:05:35 +0100

    tcl/board: add u-boot default settings for boot
    Signed-off-by: Ozan Durgut <ozan.durgut@analog.com>

---
+++ tcl/tools/u-boot.tcl
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+set CURRENT_DIR [file dirname [info script]]
+source [file join $CURRENT_DIR "elf_parser.tcl"]
+source [file join $CURRENT_DIR "auto_boot.tcl"]
+
+set AUTOBOOT_FILES {
+    {"u-boot-spl" "board_init_r"}
+    {"u-boot"     ""}
+}
+
+global _TARGETNAME
+$_TARGETNAME configure -event reset-init {
+    after 150 { _autoboot_logic }
+}
+
+init
+reset init
