7cc3a1dbcdfbdc54a0ad7fc29dee90d6adad2252
Ozan Durgut <ozan.durgut@analog.com>
2026-01-16 13:44:25 +0100

    tcl/tools: add generic interface for autoboot
    Introduce a generic boot script that accepts a BOOT_SEQ
    list. This enables multi stage booting without relying
    on hard-coded file sequences.
    
    Signed-off-by: Ozan Durgut <ozan.durgut@analog.com>

---
+++ tcl/tools/boot.tcl
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+set CURRENT_DIR [file dirname [info script]]
+source [file join $CURRENT_DIR "elf_parser.tcl"]
+source [file join $CURRENT_DIR "auto_boot.tcl"]
+
+if {![info exists BOOT_SEQ]} {
+    echo "========================================================================"
+    echo " ERROR: No boot sequence provided!"
+    echo "========================================================================"
+    echo " You are running generic_boot.tcl but did not provide the file list."
+    echo " Please pass the 'BOOT_SEQ' variable using the -c flag."
+    echo ""
+    echo " Example:"
+    echo "   openocd ... -c \"set BOOT_SEQ { {<path/to/firmware> <breakpoint>} {<path/to/firmware> <<breakpoint>>} }\" -f boot.tcl"
+    echo "========================================================================"
+    shutdown
+}
+
+set AUTOBOOT_FILES $BOOT_SEQ
+
+global _TARGETNAME
+$_TARGETNAME configure -event reset-init {
+    # 150ms delay to ensure stability after reset
+    after 150 { autoboot_elf }
+}
+
+init
+reset init
+
